d87779b13981942f243009fb81646fb234004b62
Nikolaj Andersson <nikolajandersson@gmail.com>
2018-05-04 15:46:31 +0200

    added test sounds

--- PEAQTest.m
+++ PEAQTest.m
@@ -1,6 +1,6 @@
-%clear; clc; 
+clear; clc; 
 
-ref = '2ref48000.wav'
-test = '2test48000.wav'
+ref = 'diodeTrapezoidal-AAon-guitar-30g-16x48000.wav' % 16 times oversampling
+test = 'diodeTrapezoidal-AAon-guitar-30g-4x48000.wav' % 4 times oversampling
 
 [odg, movb] = PQevalAudio_fn(ref, test)
